7c6fff518dc381e0fd06febc8308fcdc3ef7a337
skanditravels <samuel.higberg@skanditravels.com>
2026-07-29 13:23:28 -0400

    Update index.js

--- src/backend/domains/support/index.js
+++ src/backend/domains/support/index.js
@@ -1,3 +1,3 @@
-export * from "backend/domains/support/service";
-export * from "backend/domains/support/repository";
-export * from "backend/domains/support/mapper";
+export * from "src/backend/domains/support/service";
+export * from "src/backend/domains/support/repository";
+export * from "src/backend/domains/support/mapper";
