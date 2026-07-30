814f34577b6e55138176d42bf0d24883003b1f35
skanditravels <samuel.higberg@skanditravels.com>
2026-07-30 12:18:25 -0400

    Fix import path for requireCustomerContext

--- src/backend/customerHeader.web.js
+++ src/backend/customerHeader.web.js
@@ -1,5 +1,5 @@
 import { webMethod, Permissions } from "wix-web-module";
-import { requireCustomerContext } from "src/backend/core/authContext";
+import { requireCustomerContext } from "backend/core/authContext";
 
 export const getCustomerHeaderSession = webMethod(
   Permissions.Anyone,
