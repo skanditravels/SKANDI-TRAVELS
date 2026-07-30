cfb40b2c497c0c2d9065a89ec06bb3233d829c0e
skanditravels <samuel.higberg@skanditravels.com>
2026-07-30 13:51:43 -0400

    Update Careers Portal.k14if.js

--- src/pages/Careers Portal.k14if.js
+++ src/pages/Careers Portal.k14if.js
@@ -2,7 +2,7 @@ import {
   requestApplicantPortalCode,
   verifyApplicantPortalCode,
   getApplicantPortalData
-} from "src/backend/careersService.web";
+} from "backend/careersService.web";
 
 import wixLocation from "wix-location-frontend";
 
