417990129d0a4014ef6ffa5f6a52b1294435b219
skanditravels <samuel.higberg@skanditravels.com>
2026-08-24 20:39:02 -0400

    Update Uniform Center.lukfc.js

--- src/pages/Uniform Center.lukfc.js
+++ src/pages/Uniform Center.lukfc.js
@@ -1,12 +1,12 @@
 import wixLocation from "wix-location";
-import { getStaffPortalSession } from "src/backend/RIA/staffPortalAuth.web";
+import { getStaffPortalSession } from "backend/RIA/staffPortalAuth.web";
 
 import {
   getUniformEmployeeBootstrap,
   submitUniformEmployeeOrder,
   acknowledgeUniformPolicy
-} from "src/backend/uniformCenterCms.web";
-import { bindInternalHtmlBridge } from 'src/public/internalHtmlBridge';
+} from "backend/uniformCenterCms.web";
+import { bindInternalHtmlBridge } from 'public/internalHtmlBridge';
 import {
   getHrSession,
   listStaff,
@@ -15,13 +15,13 @@ import {
   generateSkId,
   printStaffBadge,
   getStaffHrReports,
-} from 'src/backend/RIA/staffHR.web';
+} from 'backend/RIA/staffHR.web';
 import {
   savePayrollProfile,
   createPayrollPeriod,
   calculatePayrollRun,
   finalizePayrollRun,
-} from 'src/backend/RIA/staffPayroll.web';
+} from 'backend/RIA/staffPayroll.web';
 
 const HR_TYPES = new Set([
   'HR_READY', 'HR_REFRESH', 'HR_SAVE_STAFF', 'HR_DEACTIVATE', 'HR_REACTIVATE',
