049293f208d0d2edd93b9bde2c2ab5b191a03423
skanditravels <samuel.higberg@skanditravels.com>
2026-09-08 06:59:46 -0400

    Update Uniform Control.yx92b.js

--- src/pages/Uniform Control.yx92b.js
+++ src/pages/Uniform Control.yx92b.js
@@ -15,7 +15,7 @@ import {
   adminAdjustUniformWallet,
   adminDeleteUniformItem,
   adminUploadUniformImage
-} from "backend/uniformCenterCms.web";
+} from "backend/uniformCenterSupabase.web";
 
 const HTML_ID = "#uniformControlEmbed";
 const CHILD_SOURCE = "SKANDI_UNIFORM_ADMIN";
