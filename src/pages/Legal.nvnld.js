7fb0b16c8b7f142daa44672ba6231015810a57d4
skanditravels <samuel.higberg@skanditravels.com>
2026-09-14 14:34:51 -0400

    Handle LEGAL_HUB_HEIGHT message to adjust embed height

--- src/pages/Legal.nvnld.js
+++ src/pages/Legal.nvnld.js
@@ -1,12 +1,15 @@
 const EMBED_ID = "#legalHubEmbed";
 const HTML_SOURCE = "SKANDI_LEGAL_HUB";
 
+
 $w.onReady(function () {
   const embed = $w(EMBED_ID);
+
 
   embed.onMessage((event) => {
     const message = event.data || {};
     if (message.source && message.source !== HTML_SOURCE) return;
+
 
     if (message.type === "LEGAL_HUB_HEIGHT") {
       const height = Number(message.payload?.height || 0);
