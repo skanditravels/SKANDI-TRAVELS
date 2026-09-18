8f1a9c9ebe31341730c983f06e8c01714504f1ca
skanditravels <samuel.higberg@skanditravels.com>
2026-09-18 07:05:14 -0400

    Update Checkout.lof54.js

--- src/pages/Checkout.lof54.js
+++ src/pages/Checkout.lof54.js
@@ -10,7 +10,7 @@ import {
   updateStoreCartLineItem,
   removeStoreCartLineItem,
   prepareStorePayment
-} from "backend/storeCheckout.web";
+} from "backend/storeCheckout.web.js";
 
 const EMBED_ID =
   "#storeCheckoutEmbed";
