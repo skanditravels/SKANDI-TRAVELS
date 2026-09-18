4dc2bb746818483c8dd1949524c7cd1ab7812b8f
skanditravels <samuel.higberg@skanditravels.com>
2026-09-18 07:02:23 -0400

    Update ALTEA Launchpad.ajs4y.js

--- src/pages/ALTEA Launchpad.ajs4y.js
+++ src/pages/ALTEA Launchpad.ajs4y.js
@@ -8,7 +8,7 @@ import wixLocationFrontend from "wix-location-frontend";
 import {
   getStaffPortalSession,
   getAlteaLaunchpadApps
-} from "backend/SKANDI_CORE/staffAuth.web";
+} from "backend/SKANDI_CORE/staffAuth.web.js";
 
 
 const EMBED_IDS = ["#alteaOpsEmbed", "#alteaLaunchpadEmbed", "#alteaMasterEmbed"];
