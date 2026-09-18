b63fd139f70656d9dbfa6bc9459809bca4361ee6
skanditravels <samuel.higberg@skanditravels.com>
2026-09-18 07:08:41 -0400

    Update Home.n73w8.js

--- src/pages/Home.n73w8.js
+++ src/pages/Home.n73w8.js
@@ -7,7 +7,7 @@ import {
   searchUnifiedOffers,
   createBookingCartFromOffer
 } from "backend/SKANDI_CORE/customerBooking.web";
-import { getHomeContent, getHomeSearchLocations } from "backend/SKANDI_CORE/homeContent.web";
+import { getHomeContent, getHomeSearchLocations } from "backend/SKANDI_CORE/homeContent.web.js";
 import { searchLiveStays } from "backend/SKANDI_CORE/customerBooking.web";
 import { APP_ROUTES } from "public/siteMap.js";
 import { openCustomerLogin } from "public/customerAuthUi.js";
