55b45f5e688bab4ed5cbb53639e8b0c09ecab096
skanditravels <samuel.higberg@skanditravels.com>
2026-09-21 01:05:16 -0400

    Fix import path for internalSearch.js

--- src/backend/SKANDI_CORE/internalSearch.web.js
+++ src/backend/SKANDI_CORE/internalSearch.web.js
@@ -1,5 +1,5 @@
 import { Permissions, webMethod } from "@wix/web-methods";
-import { runInternalGlobalSearchCore } from "backend/SKANDI_CORE/internalSearch.js";
+import { runInternalGlobalSearchCore } from "backend/SKANDI_CORE/internalSearch";
 
 
 export const runInternalGlobalSearch = webMethod(Permissions.SiteMember, runInternalGlobalSearchCore);
