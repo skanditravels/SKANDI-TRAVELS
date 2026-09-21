e39bd18002e5898e01fbcf7b477cf9afd2dea5ea
skanditravels <samuel.higberg@skanditravels.com>
2026-09-21 02:58:24 -0400

    Fix import path for network map data

--- src/pages/Our Network.wfe5g.js
+++ src/pages/Our Network.wfe5g.js
@@ -1,4 +1,4 @@
-import { getPublicNetworkMapData } from "backend/SKANDI_CORE/networkMap.web.js";
+import { getPublicNetworkMapData } from "backend/SKANDI_CORE/networkMap.web.";
 
 
 const HTML_ID = "#htmlSkandiMap";
